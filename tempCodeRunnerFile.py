a5bfe236611793bfc0f3300e8539555be761dc4e
SJoshi1312 <shreyasjoshi131207@gmail.com>
2026-09-11 19:17:41 +0530

    //Deleted_tempCodeRunnerFile.py_unnecessary//

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    x = x_test[i].reshape(784, 1)
-        label = y_test[i]
-
-        output = nn.forward(x)
-        prediction = np.argmax(output)
-
-        print("Actual:", label, "Predicted:", prediction)
